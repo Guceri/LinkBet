aeab562481c0c6298d18d6cbfac860f776ce3ac4
Guceri <guceri@gmail.com>
2021-05-19 16:44:33 -0400

    updates

--- truffle-config.js
+++ truffle-config.js
@@ -1,7 +1,8 @@
 require('babel-register');
 require('babel-polyfill');
+require('dotenv').config();
 const HDWalletProvider = require("@truffle/hdwallet-provider");
-var mnemonic = `crunch ecology what exhibit motor tray obvious absorb parade loud frozen mixture`
+const mnemonic = process.env.MNEMONIC;
 
 module.exports = {
   networks: {
@@ -9,9 +10,9 @@ module.exports = {
       host: "127.0.0.1",
       port: 7545,
       network_id: "*" // Match any network id
-    }, 
+    },
     rinkeby: {
-      provider: () => new HDWalletProvider(mnemonic, `https://rinkeby.infura.io/v3/635f1ee59b9b46a88f622162a7bb275d`),
+      provider: () => new HDWalletProvider(mnemonic, `https://rinkeby.infura.io/v3/${process.env.INFURA_API_KEY}`),
       network_id: 4,       
       gas: 9500000,        
       confirmations: 0,    
@@ -22,18 +23,14 @@ module.exports = {
   contracts_directory: './src/contracts/',
   contracts_build_directory: './src/abis/',
   compilers: {
-
-
     solc: {
-      version: "0.6.6",
+      version: "0.8.0",
       optimizer: {
         enabled: true,
         runs: 200
       },
-      evmVersion: 'istanbul'
+      evmVersion: 'istanbul' //berlin is experimental
     }
-
-
-
   }
 }
+
